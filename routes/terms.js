7d228ff5752ba142d50f143071828ff4d72d0a99
Shoniwa Marovatsanga <stmarovatsanga@gmail.com>
2017-03-16 11:14:49 +0000

    Add add definition form and function

--- routes/terms.js
+++ routes/terms.js
@@ -3,53 +3,87 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  var db = req.db;
+    var db = req.db;
     var collection = db.get('termcollection');
-    collection.find({},{},function(e,docs){
-        res.render('index', { term : docs });
+    collection.find({}, {}, function(e, docs) {
+        res.render('index', {
+            term: docs
+        });
     });
 });
 
 router.get('/show/:id', function(req, res) {
-  var db = req.db;
-  console.log(req.body);
+    var db = req.db;
+
     var collection = db.get('termcollection');
-    collection.findOne({'_id': req.params.id },function(e,docs){
-      console.log(docs);
-        res.render('show', { term : docs });
+    collection.findOne({
+        '_id': req.params.id
+    }, function(e, docs) {
+        console.log(docs);
+        res.render('show', {
+            term: docs
+        });
     });
 });
 
 /* GET term page. */
 
 /* POST to add new term. */
-router.post('/newterm', function(req, res){
-  var db = req.db;
+router.post('/newterm', function(req, res) {
+    var db = req.db;
+    // Get our form values, rely on the name attributes
+    var term = req.body.term;
+    var summary = req.body.summary;
+    var definition = req.body.definition;
+    var source = req.body.source;
 
-  // Get our form values, rely on the name attributes
-  var term = req.body.term;
-  var summary = req.body.summary;
-  var definition = req.body.definition;
-  var source = req.body.source;
+    // Set our collection
+    var collection = db.get('termcollection');
 
-  // Set our collection
-  var collection = db.get('termcollection');
-
-  // Submit to the db
-  collection.insert({
-    "term" : term,
-    "summary" : summary,
-    "definitions" : [
-      { "definition" : definition, "source" : source, "rating": 0 }
-    ]
-  }, function (err, doc) {
-    if (err) {
-      res.send("Could not add information to the database");
-    }
-    else {
-      res.redirect("/");
-    }
-  });
+    // Submit to the db
+    collection.insert({
+        "term": term,
+        "summary": summary,
+        "definitions": [{
+            "definition": definition,
+            "source": source,
+            "rating": 0
+        }]
+    }, function(err, doc) {
+        if (err) {
+            res.send("Could not add information to the database");
+        } else {
+            res.redirect("/");
+        }
+    });
 });
 
-module.exports = router;
+/* POST to add new definition to term. */
+router.post('/newdefinition', function(req, res) {
+    var db = req.db;
+    // Get our form values, rely on the name attributes
+    var definition = req.body.definition;
+    var source = req.body.source;
+    var term = req.body.term
+    // Set our collection
+    var collection = db.get('termcollection');
+    // Submit to the db
+    collection.update({"term": term}, {'$push':{"definitions": {
+        "definition": definition,
+        "source": source,
+        "rating": 0
+    }}}, function(err, doc) {
+        if (err) {
+            res.send("Could not add information to the database");
+        } else {
+            collection.findOne({
+                'term': term
+            }, function(e, docs) {
+                res.redirect("/show/" + docs._id);
+            });
+        }
+    });
+});
+
+
+        module.exports = router;
