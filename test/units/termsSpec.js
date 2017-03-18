0b58900928499982534676a9a2b4725adaca5b29
kathicks <kmhicks92@gmail.com>
2017-03-18 16:36:27 +0000

    Added test for updating definitions

--- test/units/termsSpec.js
+++ test/units/termsSpec.js
@@ -8,6 +8,7 @@ describe('Developers Dictionary', function() {
   var db_test;
   var terms;
   var data;
+  var definition;
 
   beforeEach(function() {
     db_test = monk('localhost:27017/developers-dictionary-test');
@@ -24,29 +25,61 @@ describe('Developers Dictionary', function() {
     		}
     	]
     };
+
+    definition = {
+      "definition" : "AngularJS (commonly referred to as 'Angular.js') is a JavaScript-based open-source front-end web application framework mainly maintained by Google and by a community of individuals and corporations to address many of the challenges encountered in developing single-page applications.",
+      "source" : "https://en.wikipedia.org/wiki/AngularJS",
+      "rating" : 0
+    };
   });
 
   afterEach(function() {
     return terms.remove();
   });
 
-  it('should connect to the testing database', function(done) {
-    should.exists(db_test);
-    done();
-  });
 
-  it('has a collection called termcollection', function(done) {
-    should.exists(terms);
-    done();
-  });
-
-  it('can add a term to the termcollection', function(done) {
-    terms.insert(data, function(err, insertion) {
-      if(err) return done(err);
-      should.exists(insertion);
+  describe('Connecting to the database', function() {
+    it('should connect to the testing database', function(done) {
+      should.exists(db_test);
       done();
     });
-    terms.find({}).should.eventually.have.length(1);
+    it('has a collection called termcollection', function(done) {
+      should.exists(terms);
+      done();
+    });
+  });
+
+  describe('Adding to the database', function() {
+    it('can add a term to the termcollection', function(done) {
+      terms.insert(data, function(err, insertion) {
+        if(err) return done(err);
+        should.exists(insertion);
+        done();
+      });
+      terms.find({}).should.eventually.have.length(1);
+    });
+  });
+
+  describe('Updating the database', function() {
+
+    beforeEach(function(done) {
+      terms.insert(data, function(err, insertion) {
+        if(err) return done(err);
+        done();
+      });
+    });
+
+    it('can add an additional definition to terms in the termcollection', function(done) {
+      terms.update({'term':'Angular'}, { '$push':{"definitions": definition} }, function(err, update) {
+        if(err) return done(err);
+        should.exists(update);
+        done();
+      });
+      terms.find({}, {}, function(err, docs) {
+        docs[0].definitions.should.have.length(2);
+      });
+    });
+
   });
 
 });
