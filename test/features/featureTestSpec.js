0c9d3234505c6fa63482df7aacf6d2f5f6821c1f
Shoniwa Marovatsanga <stmarovatsanga@gmail.com>
2017-03-18 17:52:54 +0000

    add validations testing

--- test/features/featureTestSpec.js
+++ test/features/featureTestSpec.js
@@ -1,7 +1,5 @@
 process.env.NODE_ENV = "test";
 
-var express = require('express');
-var app = express();
 var chai = require('chai');
 var chaiHttp = require('chai-http');
 var http = require('http');
@@ -21,10 +19,18 @@ chai.use(chaiHttp);
 
 describe('index page', function() {
 
-  var term = "AAX";
+  var term = "AJAX";
   var summary = "Asynchronous JavaScript and XML. It is the use of the XMLHttpRequest object.";
   var definition = "Mocha is a JavaScript test framework running on node.js, featuring browser support, asynchronous testing, test coverage reports, and use of any assertion library.";
   var source = "https://en.wikipedia.org/wiki/Mocha_(JavaScript_framework)";
+  var termcollection = db_test.get("termcollection");
+
+  var addingTerm = function(browser, term, summary, definition, source) {
+    browser.clickLink('a#newTerm');
+    browser.fill('term', term);
+    browser.fill('summary', summary);
+  };
+
 
   before(function(){
     this.server = http.createServer(server).listen(8000);
@@ -37,6 +43,7 @@ describe('index page', function() {
 
   beforeEach(function(done) {
     this.browser.visit('/', done);
+    termcollection.remove({})
   });
 
   it('should have a navbar', function() {
@@ -46,7 +53,6 @@ describe('index page', function() {
 
   it('should allow to add a new term', function(done) {
     var browser = this.browser;
-    assert.ok(browser.success);
     browser.clickLink('a#newTerm');
     browser.assert.element('#addTerm');
     browser.fill('term', term);
@@ -54,39 +60,33 @@ describe('index page', function() {
     browser.pressButton('Add').then(function() {
       assert.ok(browser.success);
       assert.equal(browser.text('p#successAlert'), 'Successfully created a new term!');
-      console.log(browser.text('.alert'))
     }).then(done, done);
-
   });
 
-  it('should list the terms', function() {
-
-  });
-
-
-  after(function(done) {
-     this.server.close(done);
-   });
-
-});
-
-describe('show term page', function() {
-
-  before(function(){
-    this.server = http.createServer(server).listen(8000);
-    this.browser = new Browser({
-      site: 'http://localhost:8000'
-    });
-  });
-
-
-  it('should allow to add a new term', function() {
-
-  });
-
-  it('should list the terms', function() {
-
-  });
+  describe("validations", function() {
+    it("can't add a one letter term", function(done) {
+      var browser = this.browser;
+      browser.clickLink('a#newTerm');
+      browser.assert.element('#addTerm');
+      browser.fill('term', "A");
+      browser.fill('summary', summary);
+      browser.pressButton('Add').then(function() {
+        assert.ok(browser.success);
+        assert.equal(browser.text('p#warningAlert'), 'Must be between 2 and 30 characters long');
+      }).then(done, done);
+    })
+    it("can't add a one letter summary", function(done) {
+      var browser = this.browser;
+      browser.clickLink('a#newTerm');
+      browser.assert.element('#addTerm');
+      browser.fill('term', term);
+      browser.fill('summary', "summary");
+      browser.pressButton('Add').then(function() {
+        assert.ok(browser.success);
+        assert.equal(browser.text('p#warningAlert'), 'Must be between 25 and 80 characters long');
+      }).then(done, done);
+    })
+  })
 
   after(function(done) {
      this.server.close(done);
--- test/specHelper.js
+++ test/specHelper.js
@@ -1,7 +1,5 @@
 var addingTerm = function(browser, term, summary, definition, source) {
-  browser.clickLink('a.addTerm');
+  browser.clickLink('a#newTerm');
   browser.fill('term', term);
   browser.fill('summary', summary);
-  browser.pressButton('Add');
-  browser.clickLink('a.showPage');
 };
