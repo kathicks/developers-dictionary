5d5cda7c745e0fefe5d68cb2bf977c74783f73e8
kathicks <kmhicks92@gmail.com>
2017-03-18 15:21:51 +0000

    Added further unit tests for inserting into the database

--- test/units/termsSpec.js
+++ test/units/termsSpec.js
@@ -1,7 +1,6 @@
 // Setting up the test database
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/developers-dictionary-test');
 
 // Packages
 var should = require('should');
@@ -11,9 +10,49 @@ var server = require('../../app');
 
 describe('Developers Dictionary', function() {
 
+  var db_test;
+  var terms;
+
+  var data;
+
+  beforeEach(function() {
+    db_test = monk('localhost:27017/developers-dictionary-test');
+    terms = db_test.get('termcollection');
+
+    data = {
+    	"term" : "Angular",
+    	"summary" : "A structural framework for dynamic web apps",
+    	"definitions" : [
+    		{
+    			"definition" : "It lets you use HTML as your template language and lets you extend HTML's syntax to express your application's components clearly and succinctly. AngularJS's data binding and dependency injection eliminate much of the code you would otherwise have to write. And it all happens within the browser, making it an ideal partner with any server technology.\r\n\r\nAngularJS is what HTML would have been, had it been designed for applications.",
+    			"source" : "https://docs.angularjs.org/guide/introduction",
+    			"rating" : 0
+    		}
+    	]
+    };
+  });
+
+  afterEach(function() {
+    return terms.remove();
+  });
+
   it('should connect to the testing database', function(done) {
-    should.exists(db);
+    should.exists(db_test);
     done();
+  });
+
+  it('has a collection called termcollection', function(done) {
+    should.exists(terms);
+    done();
+  });
+
+  it('can add a term to the termcollection', function(done) {
+    terms.insert(data, function(err, doc) {
+      if(err) return done(err);
+      
+      should.exists(doc);
+      done();
+    });
   });
 
 });
