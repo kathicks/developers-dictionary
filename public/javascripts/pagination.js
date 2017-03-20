0217c108f774a4d942dd61bbdc852943db3d9795
micawhitby <micawhitby@gmail.com>
2017-03-20 16:56:37 +0000

    spiking pagination on page as conventional methods of pagnition are not appropriate for the wheel. testing to see if we can spin the wheel to reach words in the requested letter. WIP.

---
+++ public/javascripts/pagination.js
@@ -0,0 +1,19 @@
+//on click of letter
+$( "#c" ).click(function() {
+  terms.forEach(function();
+    // find the first term in the array that has that letter
+    terms.term.findOne()
+    var word = { 'c' === str.charAt(0) }
+    //find where it sits in the array
+    var indexInArray = terms.term.indexOf(word)
+    //spin until the first segment has that index
+    do {
+      startAction();
+      });
+    });
+    }
+    while ('term-01'.text !== terms[indexInArray].term);
+    });
+    )
+  )
+});
