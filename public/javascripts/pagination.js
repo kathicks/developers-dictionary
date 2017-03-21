deb3a19febe65d3a181353e0c60335201c201c02
micawhitby <micawhitby@gmail.com>
2017-03-21 13:33:12 +0000

    started function to disable links in pagination if no words start with letter WIP.

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -1,14 +1,10 @@
+var terms;
 //on click of letter
 $( ".paginationLinks" ).click(function(event) {
   event.preventDefault();
   console.log($(this).text())
   var jumpToLetter = $(this).text()
-  $.ajax({
-    url: "/wheel",
-    method: "GET",
-  }).done(function(response) {
-    terms = response;
-    findWord = function(word) {
+    findWord = function() {
       for (var i = 0; i < terms.length; i++) {
         var searchable = terms[i].term.toLowerCase()
         if(searchable.charAt(0) === jumpToLetter){
@@ -16,13 +12,37 @@ $( ".paginationLinks" ).click(function(event) {
         }
       }
     };
-
     var rotateWheel = setInterval(function(){
          startAction();
       if ($('#term-01').text() === findWord()){
         clearInterval(rotateWheel);
       }
     }, 100);
-    });
   rotateWheel();
+});
+
+//DISABLE LINKS
+
+//on page load find all first letters of terms and put into array without duplicates
+//check pagination links against first letters of terms
+//if letter is missing then run disable function
+//disable function greys out letter in pag and doesn't allow clicking.
+
+$(document).ready(function(){
+  console.log();
+  $.ajax({
+    url: "/wheel",
+    method: "GET",
+  }).done(function(response) {
+    terms = response;
+    var letterArr = [];
+    terms.forEach(function(term){
+      letterArr.push(term.term.charAt(0).toLowerCase());
+    });
+    paginationArr = $('.paginationLinks').text().split('')
+    paginationArr.pop();
+    paginationArr.shift();
+
+    console.log(letterArr, paginationArr)
+  });
 });
